0356b7e71951a1405703965b4d9cdf23a5c93cd0
Dang Minh Duc <somethingduc@gmail.com>
2022-06-17 03:03:57 +0700

    Add project files.

---
+++ Lab3_BigSchool/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Lab3_BigSchool.Startup))]
+namespace Lab3_BigSchool
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
