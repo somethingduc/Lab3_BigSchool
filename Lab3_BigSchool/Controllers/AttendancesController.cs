e2ef91c6e3666b0af1b89ba7cd66cb25f083a9e4
Dang Minh Duc <somethingduc@gmail.com>
2022-06-24 03:16:12 +0700

    53 - 63

---
+++ Lab3_BigSchool/Controllers/AttendancesController.cs
@@ -0,0 +1,42 @@
+﻿using Lab3_BigSchool.DTO;
+using Lab3_BigSchool.Models;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Lab3_BigSchool.Controllers
+{
+    [Authorize]
+    public class AttendancesController : ApiController
+    {
+        private ApplicationDbContext _dbContext;
+
+        public AttendancesController()
+        {
+            _dbContext = new ApplicationDbContext();
+        }
+
+        [HttpPost]
+        public IHttpActionResult Attend(AttendanceDto attendanceDto)
+        {
+            var userId = User.Identity.GetUserId();
+            if (_dbContext.Attendances.Any(a => a.AttendeeId == userId && a.CourseId == attendanceDto.CourseId))
+                return BadRequest("The Attendances already exists!");
+
+            var attendance = new Attendance
+            {
+                CourseId = attendanceDto.CourseId,
+                AttendeeId = userId
+            };
+
+            _dbContext.Attendances.Add(attendance);
+            _dbContext.SaveChanges();
+
+            return Ok();
+        }
+    }
+}
